adc598e31cdd9df40c21342825fc5097549cfc7d
Rob Winch <rwinch@users.noreply.github.com>
2019-08-30 20:10:25 -0500

    Fix Saml2WebSsoAuthenticationRequestFilter Order
    Move Saml2WebSsoAuthenticationRequestFilter before all Authentication
    Filters

--- config/src/main/java/org/springframework/security/config/annotation/web/builders/FilterComparator.java
+++ config/src/main/java/org/springframework/security/config/annotation/web/builders/FilterComparator.java
@@ -73,15 +73,15 @@ final class FilterComparator implements Comparator<Filter>, Serializable {
 		filterToOrder.put(
 			"org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter",
 				order.next());
+		filterToOrder.put(
+				"org.springframework.security.saml2.serviceprovider.servlet.filter.Saml2WebSsoAuthenticationRequestFilter",
+				order.next());
 		put(X509AuthenticationFilter.class, order.next());
 		put(AbstractPreAuthenticatedProcessingFilter.class, order.next());
 		filterToOrder.put("org.springframework.security.cas.web.CasAuthenticationFilter",
 				order.next());
 		filterToOrder.put(
 			"org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter",
-				order.next());
-		filterToOrder.put(
-				"org.springframework.security.saml2.serviceprovider.servlet.filter.Saml2WebSsoAuthenticationRequestFilter",
 				order.next());
 		filterToOrder.put(
 				"org.springframework.security.saml2.serviceprovider.servlet.filter.Saml2WebSsoAuthenticationFilter",
