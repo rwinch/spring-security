01434b6f5f6e55a1e33b0974ec99c7e59cfe23fc
Rob Winch <rwinch@users.noreply.github.com>
2020-04-21 17:16:44 -0500

    saving

--- samples/boot/kotlin/build.gradle.kts
+++ samples/boot/kotlin/build.gradle.kts
@@ -1,26 +1,28 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("io.spring.convention.spring-sample-boot")
-    kotlin("jvm")
+    id("org.springframework.boot") version "2.2.6.RELEASE"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    kotlin("jvm") version "1.3.71"
     kotlin("plugin.spring") version "1.3.71"
 }
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://repo.spring.io/libs-snapshot")
+    }
 }
 
 dependencies {
-    implementation(project(":spring-security-core"))
-    implementation(project(":spring-security-config"))
-    implementation(project(":spring-security-web"))
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity5")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation(project(":spring-security-test"))
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ samples/copy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TO=$1
+FROM=boot/helloworld
+cp -r $FROM/gradle $TO
+cp $FROM/gradle.properties $TO
+cp $FROM/settings.gradle $TO
+cp $FROM/gradlew $TO
+cp $FROM/gradlew.bat $TO
