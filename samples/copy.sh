9029bb92439a3af4358035bf745bee8090752333
Rob Winch <rwinch@users.noreply.github.com>
2020-06-05 16:46:23 -0500

    copy resources and example package

--- samples/copy.sh
+++ samples/copy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 TO=$1
-FROM=javaconfig/helloworld
+FROM=javaconfig/inmemory
 cp -r $FROM/gradle $TO
 cp $FROM/gradle.properties $TO
 cp $FROM/settings.gradle $TO
 cp $FROM/gradlew $TO
 cp $FROM/gradlew.bat $TO
 mv $TO/src/integration-test $TO/src/integTest
+mv $FROM/src/main/java/example $TO/src/main/java
+mv $FROM/src/main/resources $TO/src/main/
