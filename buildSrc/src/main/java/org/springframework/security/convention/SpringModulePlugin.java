360e124dd35785bcf8f4e408a787afb114ab25a4
Rob Winch <rwinch@users.noreply.github.com>
2021-02-26 17:02:00 -0600

    saving

--- buildSrc/src/main/java/org/springframework/security/convention/SpringModulePlugin.java
+++ buildSrc/src/main/java/org/springframework/security/convention/SpringModulePlugin.java
@@ -3,6 +3,8 @@ package org.springframework.security.convention;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.JavaLibraryPlugin;
+import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.api.plugins.JavaPluginExtension;
 import org.gradle.api.plugins.PluginManager;
 
 public class SpringModulePlugin implements Plugin<Project> {
@@ -10,5 +12,11 @@ public class SpringModulePlugin implements Plugin<Project> {
 	public void apply(Project project) {
 		PluginManager plugins = project.getPluginManager();
 		plugins.apply(JavaLibraryPlugin.class);
+
+		project.getPlugins().withType(JavaPlugin.class).all((javaPlugin) -> {
+			JavaPluginExtension extension = project.getExtensions().getByType(JavaPluginExtension.class);
+			extension.withJavadocJar();
+			extension.withSourcesJar();
+		});
 	}
 }
