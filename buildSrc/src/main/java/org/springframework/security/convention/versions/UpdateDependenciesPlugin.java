c8db37cd1db76e5932673b0835569af119536d5e
Rob Winch <rwinch@users.noreply.github.com>
2021-03-24 22:19:49 -0500

    com.sun.xml.bind:jaxb-implcomponentSelectionRule

--- buildSrc/src/main/java/org/springframework/security/convention/versions/UpdateDependenciesPlugin.java
+++ buildSrc/src/main/java/org/springframework/security/convention/versions/UpdateDependenciesPlugin.java
@@ -4,10 +4,14 @@ import com.github.benmanes.gradle.versions.reporter.result.DependencyOutdated;
 import com.github.benmanes.gradle.versions.reporter.result.Result;
 import com.github.benmanes.gradle.versions.reporter.result.VersionAvailable;
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask;
+import com.github.benmanes.gradle.versions.updates.resolutionstrategy.ComponentSelectionRulesWithCurrent;
+import com.github.benmanes.gradle.versions.updates.resolutionstrategy.ComponentSelectionWithCurrent;
+import com.github.benmanes.gradle.versions.updates.resolutionstrategy.ResolutionStrategyWithCurrent;
 import groovy.lang.Closure;
 import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.component.ModuleComponentIdentifier;
 
 import java.io.File;
 import java.io.IOException;
@@ -47,8 +51,50 @@ public class UpdateDependenciesPlugin implements Plugin<Project> {
 						return null;
 					}
 				});
+				updateDependencies.resolutionStrategy(new Action<ResolutionStrategyWithCurrent>() {
+					@Override
+					public void execute(ResolutionStrategyWithCurrent resolution) {
+						resolution.componentSelection(new Action<ComponentSelectionRulesWithCurrent>() {
+							@Override
+							public void execute(ComponentSelectionRulesWithCurrent components) {
+								components.all(excludeWithRegex("(?i).*?(alpha|beta|m\\d+|rc\\d+).*", "an alpha or beta version"));
+								components.all(excludeWithRegex("(?i).*?rc\\d+.*", "a release candidate version"));
+								components.all((selection) -> {
+									String currentVersion = selection.getCurrentVersion();
+									int separator = currentVersion.indexOf(".");
+									String major = separator > 0 ? currentVersion.substring(0, separator) : currentVersion;
+									String candidateVersion = selection.getCandidate().getVersion();
+									Pattern calVerPattern = Pattern.compile("\\d\\d\\d\\d.*");
+									boolean isCalVer = calVerPattern.matcher(candidateVersion).matches();
+									if (!isCalVer && !candidateVersion.startsWith(major)) {
+										selection.reject("Cannot grade to new Major Version");
+									}
+								});
+								components.all((selection) -> {
+									ModuleComponentIdentifier candidate = selection.getCandidate();
+									if ("org.apache.directory.server" .equals(candidate.getGroup()) && !candidate.getVersion().equals(selection.getCurrentVersion())) {
+										selection.reject("org.apache.directory.server has breaking changes in newer versions");
+									}
+								});
+								String jaxbBetaRegex = ".*?b\\d+.*";
+								components.withModule("javax.xml.bind:jaxb-api", excludeWithRegex(jaxbBetaRegex, "Reject jaxb-api beta versions"));
+								components.withModule("com.sun.xml.bind:jaxb-impl", excludeWithRegex(jaxbBetaRegex, "Reject jaxb-api beta versions"));
+							}
+						});
+					}
+				});
 			}
 		});
+	}
+
+	static Action<ComponentSelectionWithCurrent> excludeWithRegex(String regex, String reason) {
+		Pattern pattern = Pattern.compile(regex);
+		return (selection) -> {
+			String candidateVersion = selection.getCandidate().getVersion();
+			if (pattern.matcher(candidateVersion).matches()) {
+				selection.reject(candidateVersion + " is not allowed becuase it is " + reason);
+			}
+		};
 	}
 
 	static void updateDependencyInlineVersion(Project project, DependencyOutdated dependency){
